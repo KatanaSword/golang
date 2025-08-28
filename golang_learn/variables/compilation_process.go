13006851c3e659e576018ceae5e0fb7b5f16d812
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-08-28 06:41:39 +0530

    fix compilation error

---
+++ golang_learn/variables/compilation_process.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// Run the code. Notice the compilation error? It's due to invalid syntax.
+
+// Fix the compilation error in the code.
+func main() {
+	fmt.Println("The compiled textio server is starting")
+}
