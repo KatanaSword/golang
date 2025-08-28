a5c5939c2d47e19a359939c15727011537640990
Saurabh Tajane <tajsaurabh123@gmail.com>
2025-08-28 06:33:38 +0530

    fix comment issue

---
+++ golang_learn/variables/comments.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+// The new intern on the team screwed up their documentation comment.
+
+// Fix the issue.
+func main() {
+	/*
+		We are increasing the maximum message length from 140 to 280 characters.
+		Very reluctantly, I might add.
+		Users actually want to write more than 140 characters?!? Madness.
+	*/
+
+	maxMessageLength := 140
+	newMaxMessageLength := 280
+	fmt.Println("Textio is increasing the maximum message length from", maxMessageLength, "to", newMaxMessageLength, "characters")
+}
